1c9b9bf3c6880c9856408b5d3e55ba235b41a601
JunHwaPark <37788195+JunHwaPark@users.noreply.github.com>
2020-01-10 02:09:58 +0900

    GATT communication (#1)
    * GATT communication

--- app/src/main/java/com/junhwa/bleadvertising/MainActivity.java
+++ app/src/main/java/com/junhwa/bleadvertising/MainActivity.java
@@ -1,11 +1,17 @@
 package com.junhwa.bleadvertising;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
@@ -28,22 +34,33 @@ import android.widget.Toast;
 import com.pedro.library.AutoPermissions;
 import com.pedro.library.AutoPermissionsListener;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener, AutoPermissionsListener {
+import static android.bluetooth.BluetoothProfile.STATE_CONNECTED;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AutoPermissionsListener {
     //Layout components
     private TextView mText;
     private Button mAdvertiseButton;
     private Button mDiscoverButton;
+    private Button connectButton;
     //Layout components
 
     //common components
     BluetoothManager bluetoothManager = null;
     BluetoothAdapter adapter = null;
+
+
+
+    BluetoothGatt bluetoothGatt = null;
+    BluetoothGattServer gattServer = null;
+
+    BluetoothDevice device = null;
+    UUID exUuid = null;
     //common components
 
     //discover() components
@@ -53,14 +70,13 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-            //Log.d("BLE", "onScanResult");
-            if(result == null || /*result.getDevice() == null || TextUtils.isEmpty(result.getDevice().getName())*/result.getScanRecord().getServiceUuids() == null)
+            if (result == null || result.getScanRecord().getServiceUuids() == null)
                 return;
-
-            Log.d("BLE", "result != null && result.getDevice() != null && TextUtils.isEmpty(result.getDevice().getName())");
+            exUuid = result.getScanRecord().getServiceUuids().get(0).getUuid();
             StringBuilder builder = new StringBuilder(result.getScanRecord().getServiceUuids().get(0).toString());
-            //builder.append("\n").append(new String(result.getScanRecord().getServiceData(result.getScanRecord().getServiceUuids().get(0))));
             mText.setText(builder.toString());
+            device = result.getDevice();
+            Log.d("BLE", result.getDevice().getName() + result.getDevice().toString());
         }
 
         @Override
@@ -73,19 +89,80 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
             super.onScanFailed(errorCode);
         }
     };
+
+    BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i("gattCallback", "new State = Connected");
+                Log.i("gattCallback", "Attempting to start service discovery:" + bluetoothGatt.discoverServices());
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i("gattCallback", "new State = Disconnected");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.i("onServicesDiscovered", "onServicesDiscovered received: " + gatt.getServices().size());
+                for (BluetoothGattService gattService : gatt.getServices()) {
+                    Log.d("onServicesDiscovered", gattService.getUuid().toString());
+                    if (gattService.getUuid().equals(exUuid)) {
+                        for (BluetoothGattCharacteristic characteristic : gattService.getCharacteristics()) {
+                            Log.d("characteristic", characteristic.getUuid().toString());
+                        }
+                    }
+                }
+            } else {
+                Log.w("onServicesDiscovered", "onServicesDiscovered received: " + status);
+            }
+        }
+
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d("onCharacteristicRead", characteristic.toString());
+            }
+        }
+    };
     //discover() components
+
+    BluetoothGattServerCallback gattServerCallback = new BluetoothGattServerCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            //super.onConnectionStateChange(device, status, newState);
+            switch (newState) {
+                case STATE_CONNECTED:
+                    final String str = device.toString();
+                    if (str != null)
+                        mText.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mText.append("\n" + str);
+                            }
+                        });
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        long Uuid1 = 0x0042532D00020101L;//수집기
+        long Uuid2 = 0x0201130B050C1E2DL;
+        UUID uuid = new UUID(Uuid1, Uuid2);
+        exUuid = uuid;
+
         mText = findViewById(R.id.text);
         mDiscoverButton = findViewById(R.id.discover_btn);
         mAdvertiseButton = findViewById(R.id.advertise_btn);
+        connectButton = findViewById(R.id.connect_btn);
 
         mDiscoverButton.setOnClickListener(this);
         mAdvertiseButton.setOnClickListener(this);
+        connectButton.setOnClickListener(this);
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, "this device does not support BLE", Toast.LENGTH_SHORT).show();
@@ -95,153 +172,42 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         adapter = bluetoothManager.getAdapter();
 
-        if(!adapter.isMultipleAdvertisementSupported()) {
+        if (!adapter.isMultipleAdvertisementSupported()) {
             Toast.makeText(this, "Multiple advertisement not supported", Toast.LENGTH_LONG).show();
             mAdvertiseButton.setEnabled(false);
             mDiscoverButton.setEnabled(false);
         }
 
         mBluetoothLeScanner = adapter.getBluetoothLeScanner();
+        Set<BluetoothDevice> devices =  adapter.getBondedDevices();
+        Iterator iterator = devices.iterator();
+        while (iterator.hasNext()){
+            BluetoothDevice device = (BluetoothDevice) iterator.next();
+            Log.d("bonded", device.getName() + "/" + device.getAddress());
+        }
 
         AutoPermissions.Companion.loadAllPermissions(this, 101);
-
-        Toast.makeText(this, android.os.Build.MODEL, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.discover_btn)
+        if (v.getId() == R.id.discover_btn)
             discover();
-        else if(v.getId() == R.id.advertise_btn)
+        else if (v.getId() == R.id.advertise_btn)
             advertise();
+        else if (v.getId() == R.id.connect_btn)
+            connect();
+    }
+
+    private void connect() {
+        bluetoothGatt = device.connectGatt(getApplicationContext(), false, gattCallback, 2);
+
     }
 
     private void discover() {
         ScanFilter filter = new ScanFilter.Builder()
-                //.setServiceUuid(new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid))))
                 .build();
-        List<ScanFilter> filters = new List<ScanFilter>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(@Nullable Object o) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Iterator<ScanFilter> iterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @NonNull
-            @Override
-            public <T> T[] toArray(@NonNull T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(ScanFilter scanFilter) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(@Nullable Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(@NonNull Collection<? extends ScanFilter> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, @NonNull Collection<? extends ScanFilter> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public ScanFilter get(int index) {
-                return null;
-            }
-
-            @Override
-            public ScanFilter set(int index, ScanFilter element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, ScanFilter element) {
-
-            }
-
-            @Override
-            public ScanFilter remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(@Nullable Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(@Nullable Object o) {
-                return 0;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<ScanFilter> listIterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<ScanFilter> listIterator(int index) {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public List<ScanFilter> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
+        List<ScanFilter> filters = new ArrayList<>();
         filters.add(filter);
 
         ScanSettings settings = new ScanSettings.Builder()
@@ -255,7 +221,7 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
             public void run() {
                 mBluetoothLeScanner.stopScan(mScanCallback);
             }
-        }, 10000);
+        }, 3000);
     }
 
     private void advertise() {
@@ -263,20 +229,20 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         AdvertiseSettings settings = new AdvertiseSettings.Builder()
                 .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
                 .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-                .setConnectable(false)
+                .setConnectable(true)
                 .build();
 
-        long Uuid1 = 0x0052532D00020101L;
-        long Uuid2 = 0x0103130B050C1E2DL;
-        UUID Uuid = new UUID(Uuid1, Uuid2);
+        long Uuid1 = 0x0042532D00020101L;//수집기
+        long Uuid2 = 0x0201130B050C1E2DL;
+        UUID uuid = new UUID(Uuid1, Uuid2);
+        exUuid = uuid;
 
-        ParcelUuid pUuid = new ParcelUuid(Uuid);
-        //ParcelUuid pUuid = new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid)));
+        ParcelUuid pUuid = new ParcelUuid(uuid);
+        adapter.setName("test");
 
         AdvertiseData data = new AdvertiseData.Builder()
-                .setIncludeDeviceName(false)
+                .setIncludeDeviceName(true)
                 .addServiceUuid(pUuid)
-                //.addServiceData(pUuid, "Data".getBytes())
                 .build();
 
         AdvertiseCallback advertiseCallback = new AdvertiseCallback() {
@@ -294,6 +260,18 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         };
 
         advertiser.startAdvertising(settings, data, advertiseCallback);
+
+        gattServer = bluetoothManager.openGattServer(getApplicationContext(), gattServerCallback);
+        BluetoothGattService service = new BluetoothGattService(exUuid, BluetoothGattService.SERVICE_TYPE_PRIMARY);
+        BluetoothGattCharacteristic characteristic
+                = new BluetoothGattCharacteristic(UUID.fromString("453d1ed7-9c6a-47e6-b774-4a736be00baa"),
+                BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
+        BluetoothGattCharacteristic characteristic2
+                = new BluetoothGattCharacteristic(UUID.fromString("453d1ed7-9c6a-47e6-b774-4a736be00bab"),
+                BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
+        service.addCharacteristic(characteristic);
+        service.addCharacteristic(characteristic2);
+        gattServer.addService(service);
     }
 
     @Override
